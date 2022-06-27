1d0b4e9ab3bf0c42e942b0b9c012bd4ff5a34d33
Pablo <63462690+pablo-maroto@users.noreply.github.com>
2022-06-26 23:21:50 -0300

    Add files via upload

---
+++ noticias.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html >
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Programador web con PHP y MySQL</title>
+<link href="estilos.css" rel="stylesheet"/>
+</head>
+
+<body>
+<section id="contenedor">
+	<header>
+    	<nav id="botonera_principal">
+        	<ul>
+            	<li><a href="index.php">Home</a></li><li><a href="catalogo.php">Catálogo</a></li><li><a href="noticias.php">Noticias</a></li><li><a href="clientes.php">Clientes</a></li><li><a href="contacto.php">Contáctenos</a></li>
+            </ul>
+        </nav>
+        <div id="marca">
+        	<h1>Programador web con PHP y MySQL</h1>
+        </div>
+    </header>
+    <section id="contenido">
+    <h2>Noticias</h2>
+    
+    </section>
+
+<footer>
+Desarrollado por <a href="http://www.elearning-total.com" target="_new">Programador web con PHP y MySQL</a></footer>
+</section>
+</body>
+</html>
